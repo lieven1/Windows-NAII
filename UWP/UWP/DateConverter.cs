37817f372f48f8ce8e54fdbc989d64209a05652f
Lieven Van Loo <lieven.vanloo@student.hogent.be>
2020-10-19 14:09:48 +0200

    basic listview implementatie

---
+++ UWP/UWP/DateConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace UWP
+{
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            DateTime dt = (DateTime)value;
+            return dt.ToString("dd MMMM yyy, HH:mm");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UWP/UWP/Models/Reis.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP.Models
+{
+    public class Reis
+    {
+        public string Naam { get; set; }
+        public DateTime Vertrek { get; set; } // test attributen
+        public DateTime Terug { get; set; }
+
+        public Reis()
+        {
+
+        }
+    }
+}
